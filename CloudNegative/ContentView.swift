5350514c1c719afbb7f98efda11735f248e4c735
maple-fm <fuma.deguchi@gmail.com>
2023-06-21 22:28:42 +0900

    feat: ライトモードとダークモードの切り替えでViewを変更する処理の追加

--- CloudNegative/ContentView.swift
+++ CloudNegative/ContentView.swift
@@ -8,101 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-
-    @FetchRequest(sortDescriptors: []) var clouds: FetchedResults<Cloud>
-    @StateObject private var viewModel = TextViewModel()
-    @State private var cloudOffset: CGFloat = UIScreen.main.bounds.width
-    @State var opacity: CGFloat = 0
+    @StateObject private var dataController = DataController()
+    @Environment(\.colorScheme) var colorScheme
 
     var body: some View {
-        ZStack {
-            Color(UIColor(named: "Sky")!)
-                .ignoresSafeArea()
-
-            Image("sun")
-                .resizable()
-                .frame(width: 200, height: 200)
-                .offset(x: -100, y:-300)
-
-            VStack {
-                ZStack {
-                    ForEach(viewModel.negatives, id: \.self) { negative in
-                        if self.opacity < 100 {
-                            ZStack {
-                                Image("cloud")
-                                    .resizable()
-                                    .frame(
-                                        width: { () -> CGFloat in
-                                            if viewModel.textValidated(negative.count) {
-                                                return 100
-                                            } else if negative.count > 20 {
-                                                return CGFloat(negative.count) * 10
-                                            } else {
-                                                return CGFloat(negative.count) * 25
-                                            }
-                                        }(),
-                                       height: { () -> CGFloat in
-                                               if viewModel.textValidated(negative.count) {
-                                                   return 80
-                                               } else if negative.count > 20 {
-                                                   return CGFloat(negative.count) * 6
-                                               } else {
-                                                   return CGFloat(negative.count) * 10
-                                               }
-                                           }())
-
-                                    Text(negative)
-                                        .frame(width: { () -> CGFloat in
-                                            if viewModel.textValidated(negative.count) {
-                                                return 80
-                                            } else if negative.count > 20 {
-                                                return CGFloat(negative.count) * 5
-                                            } else {
-                                                return CGFloat(negative.count) * 10
-                                            }
-                                        }())
-                            }
-                            .offset(x: CGFloat.random(in: -cloudOffset..<cloudOffset), y: CGFloat(Int.random(in: -100 ..< 50))*CGFloat(Int.random(in: 0 ..< 5)))
-                            .animation(.linear(duration: Double.random(in: 20..<21)).repeatForever(autoreverses: true))
-                            .opacity(Double(1-opacity/100))
-                        }
-
-                    }
-
-                    Spacer()
-                    HStack {
-                        TextField("不満", text: $viewModel.text)
-                            .padding(.trailing, 35)
-                            .padding(10)
-                            .background(Color(UIColor(named: "inputColor")!))
-                            .cornerRadius(10)
-                            .padding(30)
-                            .padding(.leading, 20)
-                            .frame(width: 370, height: 50)
-                            .offset(x: 0, y:350)
-
-                        Button(action: {
-                            viewModel.save()
-                        }) {
-                            Image("submit")
-                                .resizable()
-                                .frame(width: 30, height: 30)
-                        }
-                        .offset(x: -80, y: 350)
-                    }
-
-
-
-                }
-            }
-            .onAppear {
-                self.cloudOffset = 200
-            }
-            .onReceive(NotificationCenter.default.publisher(for: .deviceDidShakeNotification)) { _ in
-                print("シェイクしたよ")
-                opacity += 2
-                print(opacity)
-            }
+        if colorScheme == .dark {
+            DarkModeView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+        } else {
+            LightModeView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
         }
 
     }
@@ -110,17 +25,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-    }
-}
-
-extension NSNotification.Name {
-    public static let deviceDidShakeNotification = NSNotification.Name("DeviceDidShakeNotification")
-}
-
-extension UIWindow {
-    open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        super.motionEnded(motion, with: event)
-        NotificationCenter.default.post(name: .deviceDidShakeNotification, object: event)
+        LightModeView()
     }
 }
---
+++ CloudNegative/DarkModeView.swift
@@ -0,0 +1,115 @@
+//
+//  DarkModeView.swift
+//  CloudNegative
+//
+//  Created by 出口楓真 on 2023/06/17.
+//
+
+import SwiftUI
+
+struct DarkModeView: View {
+    @FetchRequest(sortDescriptors: []) var clouds: FetchedResults<Cloud>
+    @StateObject private var viewModel = TextViewModel()
+    @State private var cloudOffset: CGFloat = UIScreen.main.bounds.width
+    @State var opacity: CGFloat = 0
+
+    var body: some View {
+        ZStack {
+            Color(UIColor(named: "Night")!)
+                .ignoresSafeArea()
+
+            Image("moon")
+                .resizable()
+                .frame(width: 200, height: 200)
+                .offset(x: -100, y:-300)
+
+            VStack {
+                ZStack {
+                    ForEach(viewModel.negatives, id: \.self) { negative in
+                        if self.opacity < 100 {
+                            ZStack {
+                                Image("cloud")
+                                    .resizable()
+                                    .frame(
+                                        width: { () -> CGFloat in
+                                            if viewModel.textValidated(negative.count) {
+                                                return 100
+                                            } else if negative.count > 20 {
+                                                return CGFloat(negative.count) * 10
+                                            } else {
+                                                return CGFloat(negative.count) * 25
+                                            }
+                                        }(),
+                                       height: { () -> CGFloat in
+                                               if viewModel.textValidated(negative.count) {
+                                                   return 80
+                                               } else if negative.count > 20 {
+                                                   return CGFloat(negative.count) * 6
+                                               } else {
+                                                   return CGFloat(negative.count) * 10
+                                               }
+                                           }())
+
+                                    Text(negative)
+                                    .foregroundColor(.black)
+                                        .frame(width: { () -> CGFloat in
+                                            if viewModel.textValidated(negative.count) {
+                                                return 80
+                                            } else if negative.count > 20 {
+                                                return CGFloat(negative.count) * 5
+                                            } else {
+                                                return CGFloat(negative.count) * 10
+                                            }
+                                        }())
+                            }
+                            .offset(x: CGFloat.random(in: -cloudOffset..<cloudOffset), y: CGFloat(Int.random(in: -100 ..< 50))*CGFloat(Int.random(in: 0 ..< 5)))
+                            .animation(.linear(duration: Double.random(in: 20..<21)).repeatForever(autoreverses: true).delay(TimeInterval(Double.random(in: 0...1))))
+                            .opacity(Double(1-opacity/100))
+                        }
+
+                    }
+
+                    Spacer()
+                    HStack {
+                        TextField("不満", text: $viewModel.text)
+                            .padding(.trailing, 35)
+                            .padding(10)
+                            .background(Color(UIColor(named: "inputColor")!))
+                            .cornerRadius(10)
+                            .padding(30)
+                            .padding(.leading, 20)
+                            .frame(width: 370, height: 50)
+                            .offset(x: 0, y:350)
+
+                        Button(action: {
+                            viewModel.save()
+                        }) {
+                            Image("submit")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                        }
+                        .offset(x: -80, y: 350)
+                    }
+
+
+
+                }
+            }
+            .onAppear {
+                self.cloudOffset = 200
+            }
+            .onReceive(NotificationCenter.default.publisher(for: .deviceDidShakeNotification)) { _ in
+                print("シェイクしたよ")
+                opacity += 2
+                print(opacity)
+            }
+        }
+
+    }
+}
+
+struct DarkModeView_Previews: PreviewProvider {
+    static var previews: some View {
+        DarkModeView()
+    }
+}

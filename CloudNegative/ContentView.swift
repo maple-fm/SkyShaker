f099ad15e206d095229d7916d94305a904436b56
maple-fm <fuma.deguchi@gmail.com>
2023-06-24 19:03:40 +0900

    不要なファイルの削除

--- CloudNegative/ContentView.swift
+++ CloudNegative/ContentView.swift
@@ -8,16 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject private var dataController = DataController()
     @Environment(\.colorScheme) var colorScheme
 
     var body: some View {
         if colorScheme == .dark {
             CloudView(color: "Night", image: "moon")
-                .environment(\.managedObjectContext, dataController.container.viewContext)
         } else {
             CloudView(color: "Sky", image: "sun")
-                .environment(\.managedObjectContext, dataController.container.viewContext)
         }
 
     }

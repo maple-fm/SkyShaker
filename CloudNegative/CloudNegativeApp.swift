e89ca8c9cd13371e8537072b7b93e67775800f05
maple-fm <fuma.deguchi@gmail.com>
2023-07-03 19:07:48 +0900

    feat: スプラッシュ画面の追加

--- CloudNegative/CloudNegativeApp.swift
+++ CloudNegative/CloudNegativeApp.swift
@@ -10,9 +10,25 @@ import SwiftUI
 @main
 struct CloudNegativeApp: App {
     
+    @State private var showSplash = true
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            
+            if showSplash {
+                // スプラッシュ画面を表示
+                SplashView()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                            withAnimation(.easeInOut(duration: 0.5)) {
+                                showSplash = false // フェードアウトのトリガーとなる変数を変更
+                            }
+                        }
+                    }
+
+            } else {
+                ContentView()
+            }
         }
     }
 }
---
+++ CloudNegative/SplashView.swift
@@ -0,0 +1,22 @@
+//
+//  SplashView.swift
+//  CloudNegative
+//
+//  Created by 出口楓真 on 2023/07/03.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    
+    var body: some View {
+        Image("splash")
+            .resizable()
+            .scaledToFit()
+            
+    }
+}
+
+//#Preview {
+//    SplashView()
+//}
